d730bb850f99fbfb1d73a4c320c17765b4b5ad8e
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-30 21:03:07 +0900

    리포지터리 구현
    list오류 미해결

---
+++ QnA_Repeat/src/main/java/net/skhu/domain/User.java
@@ -0,0 +1,23 @@
+package net.skhu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String userId;
+	private String name;
+	private String email;
+	private String password;
+
+}
--- QnA_Repeat/src/main/java/net/skhu/web/UserController.java
+++ QnA_Repeat/src/main/java/net/skhu/web/UserController.java
@@ -3,25 +3,31 @@ package net.skhu.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.skhu.domain.User;
+import net.skhu.domain.UserRepository;
+
 @Controller
 public class UserController {
-	private List<User> users = new ArrayList<User>();
-
-	@PostMapping("/create")
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/user/create")
 	public String create(User user) {
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/user/list";
 		
 	}
 
-	@GetMapping("/list")
+	@GetMapping("/user/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 	
