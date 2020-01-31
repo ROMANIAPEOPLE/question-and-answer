dbb3506db8216a8a102c7e166bff835607730ca2
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-01-31 23:10:07 +0900

    URL 리팩토링
    URL 리팩토링

--- QnA_Repeat/src/main/java/net/skhu/web/UserController.java
+++ QnA_Repeat/src/main/java/net/skhu/web/UserController.java
@@ -1,34 +1,33 @@
 package net.skhu.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.skhu.domain.User;
 import net.skhu.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	UserRepository userRepository;
 	
-	@GetMapping("/user/create")
+	@PostMapping("")
 	public String create(User user) {
 		userRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 		
 	}
 
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
 	
 
