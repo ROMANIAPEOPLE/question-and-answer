04e6946bc0e4f3ff7d8b07ea026ea0fce1538a25
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-03 18:53:06 +0900

    3번째 반복 주기
    3번째 반복 주기

---
+++ question_and_answer/src/main/java/net/skhu/domain/User.java
@@ -0,0 +1,26 @@
+package net.skhu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+	
+	public void update(User user) {
+		
+	}
+	
+}
--- question_and_answer/src/main/java/net/skhu/web/UserController.java
+++ question_and_answer/src/main/java/net/skhu/web/UserController.java
@@ -4,26 +4,56 @@ package net.skhu.web;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.domain.User;
+import net.skhu.domain.UserRepository;
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println(user);
 		users.add(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+		//users/id/form 이 요청이 되면, user라는 모델에 id값을 보낸다.  어디에보내냐면, 업데이트 폼에!
+	}
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(newUser);
+		return "redirect:/users";
+	}
+	
+	
 }
