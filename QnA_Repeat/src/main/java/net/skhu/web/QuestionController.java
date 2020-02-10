691c9514b6472efa585722e2579f0a86c89afc02
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-10 19:11:58 +0900

    접근권한 설정
    제목 클릭시 상세내용과 수정/삭제 폼 + 수정/삭제의 접근권한 설정

--- QnA_Repeat/src/main/java/net/skhu/web/QuestionController.java
+++ QnA_Repeat/src/main/java/net/skhu/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +19,7 @@ import net.skhu.domain.User;
 public class QuestionController {
 	@Autowired
 	private QuestionRepository questionRepository;
-	
+
 	@GetMapping("/form")
 	public String form(HttpSession session) {
 		if (!HttpSessionUtils.isLoginUser(session)) {
@@ -32,9 +34,69 @@ public class QuestionController {
 			return "/users/loginForm";
 		}
 		User sessionUser = HttpSessionUtils.getUserFromSession(session);
-		Question newQuestion = new Question(sessionUser,title,contents);
+		Question newQuestion = new Question(sessionUser, title, contents);
 		questionRepository.save(newQuestion);
 		return "redirect:/";
 	}
 
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		Question question = questionRepository.findById(id).get();
+		model.addAttribute("question", question);
+
+		return "/qna/show";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findById(id).get();
+		
+		//글쓴이와 삭제대상이 동일한지?
+		if(!question.isSameWriter(loginUser)) {
+			return "/users/loginForm";//로긴폼으루
+		}
+		model.addAttribute("question", question);
+		return "/qna/updateForm";
+	}
+
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, String title, String contents, HttpSession session) {
+		
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findById(id).get();
+		//글쓴이와 삭제대상이 동일한지?
+		if(!question.isSameWriter(loginUser)) {
+			return "/users/loginForm";//로긴폼으루
+		}
+		question.update(title, contents);
+		questionRepository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable Long id, HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findById(id).get();
+		
+		//글쓴이와 삭제대상이 동일한지?
+		if(!question.isSameWriter(loginUser)) {
+			return "/users/loginForm";//로긴폼으루
+		}
+		
+		questionRepository.deleteById(id);
+		return "redirect:/";
+	}
 }
