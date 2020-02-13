c33229e80d92fe481cfdb2bc85f70bdac7cb8ec9
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-13 20:13:24 +0900

    user Entity 추가 (메모장 3번)
    user Entity 추가 (메모장 3번)

--- QnA_Repeat_1/src/main/java/net/skhu/domain/User.java
+++ QnA_Repeat_1/src/main/java/net/skhu/domain/User.java
@@ -1,12 +1,21 @@
 package net.skhu.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable=true ,length=20)
 	private String userId;
 	private String password;
 	private String name;
--- QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
+++ QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
@@ -1,8 +1,6 @@
 package net.skhu.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +8,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.skhu.domain.User;
+import net.skhu.domain.UserRepository;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-	private List<User> users = new ArrayList<User>();
+	@Autowired
+	private UserRepository userRepository;
 	
-	@PostMapping("create") 
+	
+	@PostMapping("/create") 
 	public String create(User user) {
-		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
-	@GetMapping("list")
+	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users",users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 
