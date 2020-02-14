b5744149002d96d76619a14b554315db99d9aacd
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-14 23:50:18 +0900

    반복-수정폼 이동까지
    반복-수정폼 이동까지

--- QnA_Repeat_1/src/main/java/net/skhu/web/HomeController.java
+++ QnA_Repeat_1/src/main/java/net/skhu/web/HomeController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
 public class HomeController {
-
-	@GetMapping("/create")
-	public String form() {
-		return "/users/form";
+	@GetMapping("")
+	public String home() {
+		return "index";
 	}
+
 }
--- QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
+++ QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,18 +17,36 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	@PostMapping("/create") 
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+
+	@PostMapping("")
 	public String create(User user) {
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
-	
-	@GetMapping("/list")
+
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
+	///////////기본창 : 회원가입 관련 , 회원가입 후 list로 이동하는 컨트롤러//
+	
+	
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user= userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	
+	
+	
 
 }
