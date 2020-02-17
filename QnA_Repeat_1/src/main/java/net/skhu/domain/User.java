f71cab1aca4c2fdd0447c100ef4bf35cfbe979be
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-02-17 19:21:43 +0900

    반복 4 - 로그인 처리
    반복 4 - 로그인 처리

--- QnA_Repeat_1/src/main/java/net/skhu/domain/User.java
+++ QnA_Repeat_1/src/main/java/net/skhu/domain/User.java
@@ -15,11 +15,19 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable=true ,length=20)
+	@Column(nullable=true ,length=20, unique=true)
 	private String userId;
 	private String password;
 	private String name;
 	private String email;
+	
+	public void update(User updateUser) {
+		this.userId=updateUser.userId;
+		this.password=updateUser.password;
+		this.name=updateUser.name;
+		this.email=updateUser.email;
+		
+	}
 	
 
 }
--- QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
+++ QnA_Repeat_1/src/main/java/net/skhu/web/UserController.java
@@ -1,5 +1,7 @@
 package net.skhu.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -42,6 +44,38 @@ public class UserController {
 		User user= userRepository.findById(id).get();
 		model.addAttribute("user", user);
 		return "/user/updateForm";
+	}
+	
+	@PostMapping("/{id}")
+	//수정하기 버튼을 눌렀을 때 요청되는 url
+	public String update(@PathVariable Long id, User updateUser) {
+		User user = userRepository.findById(id).get();
+		user.update(updateUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+	
+		if(user == null) {
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			return "redirect:/users/loginForm";
+		}
+		
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
 	}
 	
 	
